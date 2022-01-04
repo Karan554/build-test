1ff5736b464c9e2205a13c4d0583033b1075af25
Karan Bisht <bisht.karan0@gmail.com>
2022-01-04 20:23:47 +0530

    changes in mvn script

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/var/jenkins_home/workspace/Pipeline-project
 
-docker run --rm -v  "$WORKSPACE/java-app/":"/app" -v "/mnt/c/Users/Kamal Bisht/Volumes/.m2":"/root/.m2/" -w "/app" maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app/:/app -v /mnt/c/Users/Kamal Bisht/Volumes/.m2:/root/.m2/ -w /app maven:3-alpine "$@"
